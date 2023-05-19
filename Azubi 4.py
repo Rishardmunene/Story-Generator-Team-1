f0e021b0cc4a85c6e7009340f8b66cd0bdf3cc9d
RISHARD. M . BAKUNDA <richbakunda@gmail.com>
2023-05-19 14:13:39 +0300

    Create Azubi 4.py

---
+++ Azubi 4.py
@@ -0,0 +1,14 @@
+# Team 1
+
+my_story = [" This is my story ", " It has been an exciting experience ", " Thank you for all the effort you are putting in ", " I have interacted and learned from my colleagues ", " Looking forward to continue building on our skills"]
+
+
+
+print("Hello, how are you? I will be you story generator")
+name = input("Enter Name: ")
+age = input("Enter age: ")
+company = input("Enter Company name: ")
+cohort = input("Enter Cohort: ")
+course =  input("Enter the Course: ")
+
+print(f"Hi, I'm {name}. {my_story[0]} I am cloud trainee at {company} taking {course} in cohort { cohort}. {my_story[1]}. {my_story[3]}. {my_story[4]}. {my_story[2]}")
